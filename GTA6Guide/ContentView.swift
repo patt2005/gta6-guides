2af8e9d9681459c0a160d0e939cc3610a5baa7ef
Peter Grigor <petrupewdiepie@gmail.com>
2026-08-30 16:28:53 +0300

    ipad ui fixes

--- GTA6Guide/ContentView.swift
+++ GTA6Guide/ContentView.swift
@@ -4,7 +4,8 @@ struct ContentView: View {
     @State private var selectedTab = 0
     @AppStorage("hasShownOnboarding") var hasShownOnboarding: Bool = false
     @State private var showOnboarding: Bool = false
-
+    @State private var cachedSizeClass: UserInterfaceSizeClass = .compact
+    
     var body: some View {
         TabView(selection: $selectedTab) {
             HomeView()
@@ -12,30 +13,50 @@ struct ContentView: View {
                     Label("Home", systemImage: "house.fill")
                 }
                 .tag(0)
-
+                .environment(\.horizontalSizeClass, cachedSizeClass)
+                .transformEnvironment(\.horizontalSizeClass) { sizeClass in
+                    sizeClass = .compact
+                }
+            
             GuidesListView()
                 .tabItem {
                     Label("Guides", systemImage: "book.fill")
                 }
                 .tag(1)
-
+                .environment(\.horizontalSizeClass, cachedSizeClass)
+                .transformEnvironment(\.horizontalSizeClass) { sizeClass in
+                    sizeClass = .compact
+                }
+            
             MapView()
                 .tabItem {
                     Label("Map", systemImage: "map.fill")
                 }
                 .tag(2)
-
+                .environment(\.horizontalSizeClass, cachedSizeClass)
+                .transformEnvironment(\.horizontalSizeClass) { sizeClass in
+                    sizeClass = .compact
+                }
+            
             CheatCodesView()
                 .tabItem {
                     Label("Cheats", systemImage: "keyboard.fill")
                 }
                 .tag(3)
-
+                .environment(\.horizontalSizeClass, cachedSizeClass)
+                .transformEnvironment(\.horizontalSizeClass) { sizeClass in
+                    sizeClass = .compact
+                }
+            
             MoreView()
                 .tabItem {
                     Label("More", systemImage: "ellipsis")
                 }
                 .tag(4)
+                .environment(\.horizontalSizeClass, cachedSizeClass)
+                .transformEnvironment(\.horizontalSizeClass) { sizeClass in
+                    sizeClass = .compact
+                }
         }
         .accentColor(.neonPink)
         .onAppear {
